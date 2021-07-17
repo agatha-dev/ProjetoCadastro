bf4de1a820df132a32d8103f949d37a6b70eca46
Agatha <agathasantos145@gmail.com>
2021-07-17 00:10:50 -0300

    Criando arquitetura DDD

---
+++ ProjetoEstagio.Domain/Entities/Aluno.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstagio.Domain.Entities
+{
+    class Aluno 
+    {
+        public Guid IdAluno { get; set; }
+        public string Cpf { get; set; }
+        public string nome { get; set; }
+        public Guid  IdMatricula { get; set; }
+        public string Endeco { get; set; }
+        public string Email { get; set; }
+        //1 para 1
+        public Turma Turma { get; set; }
+
+
+    }
+}
---
+++ ProjetoEstagio.Domain/Entities/Turma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstagio.Domain.Entities
+{
+    class Turma
+    {
+        public Guid IdTurma { get; set; }
+        public string Curso { get; set; }
+        //1 para muitos
+        public List<Aluno> Alunos { get; set; }
+
+    }
+}
